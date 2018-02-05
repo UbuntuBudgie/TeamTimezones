8ad31693198097fe7c21733d82c91d4771175a11
HEXcube <hexcubed@gmail.com>
2018-02-05 23:26:56 +0530

    Add clocks & dates to each team member
    - Add styles to hide the redundant clocks and dates

--- assets/js/jClocksGMT-config.js
+++ assets/js/jClocksGMT-config.js
@@ -3,11 +3,62 @@
   https://www.jqueryscript.net/time-clock/jQuery-Based-Analog-And-Digital-World-Clock-jClocksGMT-js.html
 */
 
-$('#clock_hou').jClocksGMT({
+$('#timezone-uk').jClocksGMT({
   title: '',
   digital: true,
   analog: false,
   offset: '0',
+  timeformat: 'HH:mm',
+  date: true,
+  dateformat: 'YYYY-MM-DD'
+})
+
+$('#timezone-vancouver').jClocksGMT({
+  title: '',
+  digital: true,
+  analog: false,
+  offset: '-8',
+  timeformat: 'HH:mm',
+  date: true,
+  dateformat: 'YYYY-MM-DD'
+})
+
+$('#timezone-belgrade').jClocksGMT({
+  title: '',
+  digital: true,
+  analog: false,
+  offset: '+1',
+  timeformat: 'HH:mm',
+  date: true,
+  dateformat: 'YYYY-MM-DD'
+})
+
+$('#timezone-netherlands').jClocksGMT({
+  title: '',
+  digital: true,
+  analog: false,
+  offset: '+1',
+  timeformat: 'HH:mm',
+  date: true,
+  dateformat: 'YYYY-MM-DD'
+})
+
+$('#timezone-italy').jClocksGMT({
+  title: '',
+  digital: true,
+  analog: false,
+  offset: '+1',
+  timeformat: 'HH:mm',
+  date: true,
+  dateformat: 'YYYY-MM-DD'
+})
+
+$('#timezone-india').jClocksGMT({
+  title: '',
+  digital: true,
+  analog: false,
+  offset: '+5.5',
+  dst: false,
   timeformat: 'HH:mm',
   date: true,
   dateformat: 'YYYY-MM-DD'
